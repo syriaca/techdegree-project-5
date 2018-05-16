2b962df5e3c53d806ae18431d9c57f53e5cc1669
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-05-16 16:43:09 +0200

    Laptop to desktop
    changing of works station

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ request.onreadystatechange = function(event) {
     if (this.readyState === XMLHttpRequest.DONE) {
         let employeesDirectoryHTMLHeading = this.status === 200  ? 'Awesome employee startup directory' : 'No result founded';
         let employeesDirectoryHTML = '<section class="section">';
+        let employeesDirectoryModalHTML = '';
         employeesDirectoryHTML += '<header class="section-header"><h1 class="section-header__heading">'+employeesDirectoryHTMLHeading+'</h1></header>';
         if (this.status === 200) {
             let employeeData = JSON.parse(this.response);
@@ -15,26 +16,60 @@ request.onreadystatechange = function(event) {
                 let employeeLastName = employeeData.results[i].name.last;
                 let employeeEmail = employeeData.results[i].email;
                 let employeeCity = employeeData.results[i].location.city;
-                employeesDirectoryHTML += 
-                `<li class="employees-list__item">
-                    <article class="employees-list__article">
-                        <figure class="employees-list__figure">
-                            <img class="employees-list__image" src="${employeePicture}"/>
+                let employeeUsername = employeeData.results[i].login.username;
+                let employeeCell = employeeData.results[i].cell;
+                let employeeLocation = employeeData.results[i].location;
+                let employeeDOB = employeeData.results[i].dob;
+
+                employeesDirectoryModalHTML = `
+                    <div class="employee-modal">
+                        <figure class="employee-modal__figure">
+                            <img class="employee-modal__image" src="${employeePicture}"/>
                         </figure>
-                        <header class="employees-list__header">
-                            <h1 class="employees-list__heading employee__name">
+                        <header class="employees-modal__header">
+                            <h1 class="employees-modal__heading employee__name">
                                 <span class="employee__name-first">${employeeFirstName}</span>
                                 <span class="employee__name-last">${employeeLastName}</span>
                             </h1>
-                            <p class="employees-list__paragraph employee__email">${employeeEmail}</p>
-                            <p class="employees-list__paragraph employee__city">${employeeCity}</p>
-                    </article>
+                            <p class="employees-modal__paragraph employee__username">${employeeUsername}</p>
+                            <p class="employees-modal__paragraph employee__email">${employeeEmail}</p>
+                            <p class="employees-modal__paragraph employee__cell">${employeeCell}</p>
+                            <p class="employees-modal__paragraph employee__location"></p>
+                        </header>
+                    </div>`;
+
+                employeesDirectoryHTML += 
+                `<li class="employees-list__item">
+                    <a href="#" class="employees-list__link">
+                        <article class="employees-list__article">
+                            <figure class="employees-list__figure">
+                                <img class="employees-list__image" src="${employeePicture}"/>
+                            </figure>
+                            <header class="employees-list__header">
+                                <h1 class="employees-list__heading employee__name">
+                                    <span class="employee__name-first">${employeeFirstName}</span>
+                                    <span class="employee__name-last">${employeeLastName}</span>
+                                </h1>
+                                <p class="employees-list__paragraph employee__email">${employeeEmail}</p>
+                                <p class="employees-list__paragraph employee__city">${employeeCity}</p>
+                            </header>
+                        </article>
+                    </a>
                 </li>`;
+                
                 if(i % 4 === 0 ) {
                     employeesDirectoryHTML += '</ul><ul class="employees-list">';
                 }
             }
             employeeDirectory.innerHTML = employeesDirectoryHTML;
+
+            // get all links in list and add event listener in order to add a modal window
+            let employeesModalLink = document.querySelectorAll(".employees-list__link");
+            for(let i = 0; i < employeesModalLink.length; i++) {
+                employeesModalLink[i].addEventListener('click', function(e) {
+                    console.log(this);
+                });
+            }
         }
     }
 }
